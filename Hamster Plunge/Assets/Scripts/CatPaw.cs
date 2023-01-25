82779c440184ae052fdbed09b8ce72f03ef7fd65
troberts2 <troberts2@mail.bradley.edu>
2023-01-25 12:52:26 -0600

    cat paw hazard 1 done

--- Hamster Plunge/Assets/Scripts/CatPaw.cs
+++ Hamster Plunge/Assets/Scripts/CatPaw.cs
@@ -24,7 +24,13 @@ public class CatPaw : MonoBehaviour
     IEnumerator pawSwipe (){
         canAttack = false;
         anim.SetTrigger("attack");
-
+        if(AngleDir(transform.position, player.position) > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else{
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
 
         yield return new WaitForSeconds(4f);
         canAttack = true;
@@ -36,7 +42,20 @@ public class CatPaw : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
-            rb.velocity = hitForce*rb.mass*transform.right + Vector3.up*rb.velocity.y;
+            if(AngleDir(transform.position, player.position) > 0)
+            {
+                rb.velocity = hitForce*rb.mass*transform.right*-1 + Vector3.up*rb.velocity.y;
+            }
+            if(AngleDir(transform.position, player.position) < 0)
+            {
+                rb.velocity = hitForce*rb.mass*transform.right + Vector3.up*rb.velocity.y;
+            }
+            
         }
+    }
+
+    public static float AngleDir(Vector2 A, Vector2 B)
+    {
+        return -A.x * B.y + A.y * B.x;
     }
 }
