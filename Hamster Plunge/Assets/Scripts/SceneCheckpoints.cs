30404d1f6a9f4ea3c4370fc913be79b13031f5f4
NGrinstead0101 <ngrinstead@mail.bradley.edu>
2023-02-01 12:39:31 -0600

    Added Scene Checkpoints

---
+++ Hamster Plunge/Assets/Scripts/SceneCheckpoints.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class SceneCheckpoints : MonoBehaviour
+{
+    static int currentScene = 0;
+    PlayerControls playerControls;
+    InputAction advance;
+    InputAction revert;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+        playerControls.Default.Enable();
+        advance = playerControls.FindAction("AdvanceScene");
+        revert = playerControls.FindAction("revertScene");
+
+        advance.performed += ctx => ChangeScene(1);
+        revert.performed += ctx => ChangeScene(-1);
+    }
+
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(currentScene);
+    }
+
+    private void ChangeScene(int changeDir)
+    {
+        currentScene += changeDir;
+        if (currentScene < 0)
+        {
+            currentScene = 0;
+        }
+        else if (currentScene >= SceneManager.sceneCount)
+        {
+            currentScene = SceneManager.sceneCount - 1;
+        }
+        SceneManager.LoadScene(currentScene);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ChangeScene(1);
+    }
+
+   
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+}
